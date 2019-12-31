be77801f028cb22d5e7665660ca3a7344a415913
jerryzhang <jerryzhang@muchenglin.com>
2019-12-31 19:26:39 +0800

    注释

--- src/core/observer/index.js
+++ src/core/observer/index.js
@@ -34,6 +34,9 @@ export function toggleObserving (value: boolean) {
  * object's property keys into getter/setters that
  * collect dependencies and dispatch updates.
  */
+/*
+    每个被观察到对象被附加上观察者实例，一旦被添加，观察者将为目标对象加上getter\setter属性，进行依赖收集以及调度更新。
+*/
 export class Observer {
   value: any;
   dep: Dep;
@@ -46,8 +49,10 @@ export class Observer {
     def(value, '__ob__', this)
     if (Array.isArray(value)) {
       if (hasProto) {
+        /*直接覆盖原型的方法来修改目标对象*/
         protoAugment(value, arrayMethods)
       } else {
+        /*定义（覆盖）目标对象或数组的某一个方法*/
         copyAugment(value, arrayMethods, arrayKeys)
       }
       this.observeArray(value)
@@ -60,6 +65,9 @@ export class Observer {
    * Walk through all properties and convert them into
    * getter/setters. This method should only be called when
    * value type is Object.
+   */
+  /*
+      遍历每一个对象并且在它们上面绑定getter与setter。这个方法只有在value的类型是对象的时候才能被调用
    */
   walk (obj: Object) {
     const keys = Object.keys(obj)
@@ -107,6 +115,11 @@ function copyAugment (target: Object, src: Object, keys: Array<string>) {
  * returns the new observer if successfully observed,
  * or the existing observer if the value already has one.
  */
+/*
+尝试创建一个Observer实例（__ob__），
+如果成功创建Observer实例则返回新的Observer实例，
+如果已有Observer实例则返回现有的Observer实例。
+*/
 export function observe (value: any, asRootData: ?boolean): Observer | void {
   if (!isObject(value) || value instanceof VNode) {
     return
@@ -120,10 +133,16 @@ export function observe (value: any, asRootData: ?boolean): Observer | void {
     (Array.isArray(value) || isPlainObject(value)) &&
     Object.isExtensible(value) &&
     !value._isVue
+    /*
+      这里的判断是为了确保value是单纯的对象，而不是函数或者是Regexp等情况。
+      而且该对象在shouldConvert的时候才会进行Observer。
+      这是一个标识位，避免重复对value进行Observer
+    */
   ) {
     ob = new Observer(value)
   }
   if (asRootData && ob) {
+    /*如果是根数据则计数，后面Observer中的observe的asRootData非true*/
     ob.vmCount++
   }
   return ob
@@ -133,22 +152,26 @@ export function observe (value: any, asRootData: ?boolean): Observer | void {
  * Define a reactive property on an Object.
  */
 export function defineReactive (
-  obj: Object,
-  key: string,
-  val: any,
-  customSetter?: ?Function,
-  shallow?: boolean
+  obj: Object,  //对象
+  key: string,  //对象的key
+  val: any,     //监听的数据 返回的数据
+  customSetter?: ?Function, //  日志函数
+  shallow?: boolean //是否要添加__ob__ 属性
 ) {
   const dep = new Dep()
 
   const property = Object.getOwnPropertyDescriptor(obj, key)
+  // configurable属性为true，对象的属性描述可以被改变或者属性可被删除
   if (property && property.configurable === false) {
     return
   }
-
+  /*如果之前该对象已经预设了getter以及setter函数则将其取出来，新定义的getter/setter中会将其执行，保证不会覆盖之前已经定义的getter/setter。*/
   // cater for pre-defined getter/setters
   const getter = property && property.get
   const setter = property && property.set
+  // 只有在walk函数中才是args长度为2
+  // 当没有设置getter或者已有setter
+  // TODO: 这个条件存疑
   if ((!getter || setter) && arguments.length === 2) {
     val = obj[key]
   }
@@ -158,10 +181,13 @@ export function defineReactive (
     enumerable: true,
     configurable: true,
     get: function reactiveGetter () {
+      /*如果原本对象拥有getter方法则执行*/
       const value = getter ? getter.call(obj) : val
       if (Dep.target) {
+        /*进行依赖收集*/
         dep.depend()
         if (childOb) {
+          /*子对象进行依赖收集，其实就是将同一个watcher观察者实例放进了两个depend中，一个是正在本身闭包中的depend，另一个是子元素的depend*/
           childOb.dep.depend()
           if (Array.isArray(value)) {
             dependArray(value)
